59781dee348b997b31d1a27de743f426299e4e5a
AHakan <ahmethcelik@gmail.com>
2020-12-29 15:18:38 +0300

    perceptron2

--- PERCEPTRON/perceptron.py
+++ PERCEPTRON/perceptron.py
@@ -1,21 +1,26 @@
 import numpy as np
 
 class Perceptron(object):
+    # baslangic degerlerini atiyor.
     def __init__(self, input_size, lr=1, epochs=10):
         self.W = np.zeros(input_size+1)
 
         self.epochs = epochs
         self.lr = lr
 
+    # aktivasyon işlemi uygular. giriş degeri 0 dan buyukse ve eşitse 1 degilse 0 donduruyor.
     def activation_fn(self, x):
         return 1 if x >=0 else 0
 
+    # transpoze alır ve tek değer üretir bunu activasyona gonderir.
     def predict(self, x):
         x = np.insert(x, 0, 1)
         z = self.W.T.dot(x)
         a = self.activation_fn(z)
+        print(a)
         return a
 
+    # verilen değerin transpozesini alıp bunu kapıdan çıkartıyor ve değerleri olusturuyor.
     def fit(self, X, d):
         for _ in range(self.epochs):
             for i in range(d.shape[0]):
@@ -39,3 +44,36 @@ perceptron = Perceptron(input_size=2)
 perceptron.fit(X, d)
 
 print(perceptron.W)
+
+
+# 1.sorunun cevabı fonksiyonlar üzerinde yazıyor.
+
+
+# 2.sorunun cevabı olarak XOR => d = np.array([0, 1, 1, 0])
+#  [ 0. -1.  0.] sonucu dönüyor.
+
+
+# 3.soru
+
+# imza tanılaması için girilen imza dataları siyah ve beyaz dönüşümü yapılıp kullanılabilir dersek dizi:
+# [[ 1 1 1 .... 1 1 1 1]
+#  [ 0 0 0 .... 0 0 0 0]
+#  [ 1 0 0 .... 0 1 0 0]
+#  ....
+#  ....
+# ] gibi olacaktır.
+
+# bundan dolayı X = np.asarray([[[ 1 1 1 .... 1 1 1 1]
+#  [ 0 0 0 .... 0 0 0 0]
+#  [ 1 0 0 .... 0 1 0 0]
+#  ....
+#  ....
+# ]])
+
+# d = np.array([0, 0, 0, 1])
+
+# d yi de and kapısı seçersek verilerin dot çarpımları yapıldıktan sonra her imza için predict fonksiyonu ile farklı bi değer bulunur.
+# bu saye de her imza için farklı değerler oluşturulur.
+
+# 4.soru 
+# bu modelin hatası XOR gibi ilnearly separable olmayan veriler için sıkıntı oluşturur. bu veriler de doğru sonucu elde edemeyebiliriz.
